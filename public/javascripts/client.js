d01e14799df5b36c1971fe9a11a96b596f4a75c3
basketdelgado <f.basket1@gmail.com>
2017-02-22 21:06:26 +0100

    añadido avatares

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,21 @@
 var socket = io.connect();
 var nick = "";
+var avatar;
 var modal= $('<div class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">'
         +'<h5 class="modal-title">Introduce datos</h5>'
-        +'<div id="imagenes"></div>'
+        +'<div id="imagenes"><div id=culo><img src="/images/avatar1.jpg"></div>'
         +'<form id="formulario"><div class="modal-body"><input id="nick" type="text" placeholder="Nick"></div>'
         +'<div class="modal-footer"><input type="submit" class="btn btn-primary id="enviar" value="Enviar"></input></div></form></div></div></div>');
 
 $(document).ready(function(){
     $("body").append(modal);
+        $("#culo").click(function(e){
+        avatar = $(e.target).children().attr("src");
+    });
     $("#formulario").submit(function(){
         nick = $("#nick").val();
-        if(nick!=""){
+        if(nick!="" && avatar!=null){
+            $(body).append("<div>"+nick+"</div>");
             socket.emit("new user",nick);
             $(".modal").css("display","none");
         }
@@ -18,10 +23,11 @@ $(document).ready(function(){
     });
 });
 
-socket.on("new user",function(user){
+socket.on("new user",function(id,user){
     var alertUserConect = "<div id=alertaUser><p>"+user+ " se ha conectado</p></div>";
     $("body").append(alertUserConect);
-        setTimeout(function(){$("#alertaUser").css("display","none")},3000);
+    $("#usuarios").append("<div id="+id+"><span>"+user+"</span></div>");
+    setTimeout(function(){$("#alertaUser").css("display","none")},3000);
 
 });
 
@@ -30,6 +36,7 @@ $("#formularioMensaje").submit(function(){
     if(textoMensaje!=""){
         var msg = {"nick":nick,"mensaje":textoMensaje};
         socket.emit("newMesaje",msg);
+        animateScroll();
         $("#chat").append("<div><span>"+msg.nick+" </span><span>"+msg.mensaje+"</span><div>");
         $("#textoMensaje").val("")
     }
@@ -41,7 +48,12 @@ socket.on("newMesaje",function(msg){
     animateScroll();
 })
 
+socket.on("allUsers",function(users){
+    users.forEach(function(item){
+        $("#usuarios").append("<div id="+item+"><span>"+item+"</span></div>");
+    });
+});
 function animateScroll(){
     var container = $('#chat');
-    container.animate({"scrollTop": container[0].scrollHeight}, "slow");
+    container.animate({"scrollTop": container[0].scrollHeight},"faster");
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var delivery = require("delivery");
-var usuarios = [];
+var usuarios = {};
 
 app.use(express.static(path.join(__dirname, 'public')));
 var port = process.env.PORT || 3000;
@@ -20,14 +20,17 @@ http.listen(port, function(){
 io.emit('some event', { for: 'everyone' });
 
 io.on('connection', function(socket){
-  socket.on("new user", function(usuario){
-    usuarios.push(usuario);
-    socket.broadcast.emit("new user",usuario);
+  socket.on("new user", function(user){
+    user[id]=socket.id;
+    socket.emit("allUsers",usuarios);
+    socket.broadcast.emit("new user",user);
+
   });
   socket.on('newMesaje', function(msg){
     socket.broadcast.emit('newMesaje', msg);
   });
   socket.on('disconnect', function(){
+    console.log(socket.id);
     console.log('user disconnected');
   });
 });
