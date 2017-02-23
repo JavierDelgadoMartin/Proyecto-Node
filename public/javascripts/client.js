795d1b6d50640a1e169c8adc861856c4a2a3ce0b
basketdelgado <f.basket1@gmail.com>
2017-02-23 13:29:55 +0100

    añadida conexion y desconexion de usuarios

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,32 +1,19 @@
 var socket = io.connect();
 var nick = "";
 var avatar;
+var estado = "";
 var modal= $('<div class="modal"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">'
         +'<h5 class="modal-title">Introduce datos</h5>'
-        +'<div id="imagenes"><div id=culo><img src="/images/avatar1.jpg"></div>'
-        +'<form id="formulario"><div class="modal-body"><input id="nick" type="text" placeholder="Nick"></div>'
+        +'<div id="imagenes"><img src="/images/avatar1.png"><img src="/images/avatar2.png"><img src="/images/avatar3.png"><img src="/images/avatar4.png">'
+        +'<img src="/images/avatar5.png"><img src="/images/avatar6.png">'
+        +'<form id="formulario"><div class="modal-body"><input id="nick" type="text" placeholder="Nick">'
+        +'<select id=#estado><option>en linea</option><option>ausente</option><option>ocupado</option></select></div>'
         +'<div class="modal-footer"><input type="submit" class="btn btn-primary id="enviar" value="Enviar"></input></div></form></div></div></div>');
 
-$(document).ready(function(){
-    $("body").append(modal);
-        $("#culo").click(function(e){
-        avatar = $(e.target).children().attr("src");
-    });
-    $("#formulario").submit(function(){
-        nick = $("#nick").val();
-        if(nick!="" && avatar!=null){
-            $(body).append("<div>"+nick+"</div>");
-            socket.emit("new user",nick);
-            $(".modal").css("display","none");
-        }
-        return false;
-    });
-});
-
-socket.on("new user",function(id,user){
-    var alertUserConect = "<div id=alertaUser><p>"+user+ " se ha conectado</p></div>";
+socket.on("new user",function(user){
+    var alertUserConect = "<div id=alertaUser><p>"+user.name+ " se ha conectado</p></div>";
     $("body").append(alertUserConect);
-    $("#usuarios").append("<div id="+id+"><span>"+user+"</span></div>");
+    $("#usuarios").append("<div id="+user.id+"><img src="+user.avatar+"></img><span>"+user.name+"</span></div>");
     setTimeout(function(){$("#alertaUser").css("display","none")},3000);
 
 });
@@ -49,11 +36,44 @@ socket.on("newMesaje",function(msg){
 })
 
 socket.on("allUsers",function(users){
-    users.forEach(function(item){
-        $("#usuarios").append("<div id="+item+"><span>"+item+"</span></div>");
-    });
+    for (var item in users){
+        $("#usuarios").append("<div id="+users[item].id+"><img src="+users[item].avatar+"></img><span>"+users[item].name+"</span></div>");
+    };
 });
+
+socket.on("writting",function(name){
+    var alertWritting = $("<div><span>El usuario "+name+" está escribiendo</span></div>");
+    $("body").append(alertWritting);
+});
+
+socket.on("userDisconnect",function(id){
+    $("div #"+id).remove();
+});
+
 function animateScroll(){
     var container = $('#chat');
     container.animate({"scrollTop": container[0].scrollHeight},"faster");
 }
+
+function avisoEscribiendo(){
+    $("#textoMensaje").keyup(function(){
+        socket.emit("writting",nick);
+    });
+}
+
+$(document).ready(function(){
+    $("body").append(modal);
+    $("img").click(function(e){
+        avatar = $(e.target).attr("src");
+    });
+    $("#formulario").submit(function(){
+        nick = $("#nick").val();
+        estado = $("#estado").children();
+        if(nick!="" && avatar!=null){
+            $("#esteUsuario").append("<img src="+avatar+"></img><span>"+nick+"</span>");
+            socket.emit("new user",nick,avatar);
+            $(".modal").css("display","none");
+        }
+        return false;
+    });
+});
--- server.js
+++ server.js
@@ -20,17 +20,26 @@ http.listen(port, function(){
 io.emit('some event', { for: 'everyone' });
 
 io.on('connection', function(socket){
-  socket.on("new user", function(user){
-    user[id]=socket.id;
+  socket.on("new user", function(name,avatar){
+    var user = {"id":socket.id,"name":name,"avatar":avatar};
     socket.emit("allUsers",usuarios);
+    usuarios[socket.id] = {"id":socket.id,"name":name,"avatar":avatar};
     socket.broadcast.emit("new user",user);
-
   });
+
+  socket.on("writting",function(name){
+    socket.broadcast.emit("writting",name)
+  });
+
   socket.on('newMesaje', function(msg){
     socket.broadcast.emit('newMesaje', msg);
   });
+
   socket.on('disconnect', function(){
-    console.log(socket.id);
-    console.log('user disconnected');
+    if(usuarios[socket.id]!=undefined){
+      delete usuarios[socket.id];
+      socket.broadcast.emit("userDisconnect",socket.id);
+    }
   });
+
 });
